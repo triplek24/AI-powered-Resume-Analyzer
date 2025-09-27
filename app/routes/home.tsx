a61bc6334a7a4fe5c7282a81eef8eae604732008
triplek24 <karthikeyanrym@gmail.com>
2025-09-27 16:02:00 +0530

    Header and Home screen added

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,31 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Nav";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    return [
+        { title: "Resumind" },
+        { name: "description", content: "Smart feedback for your dream job!" },
+    ];
 }
 
+
 export default function Home() {
-  return <Welcome />;
+    return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+
+        <Navbar/>
+        <section className="main-section">
+            <div className="page-heading py-16">
+
+
+                <h1>
+                    Track Your Application & Resume Ratings
+                </h1>
+                <h2>
+                    Review your submission and check AI-powered feedback.
+                </h2>
+            </div>
+        </section>
+    </main>
+
+
 }
